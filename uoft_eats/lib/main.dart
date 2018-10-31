da95ea424af66a3a896a96ac706bcb5d5d3eabc2
Xingyu Wang <kara.wang@mail.utoronto.ca>
2018-10-30 22:36:13 -0400

    Updated main to include new screens

--- uoft_eats/lib/main.dart
+++ uoft_eats/lib/main.dart
@@ -1,16 +1,26 @@
 import 'package:flutter/material.dart';
 
+import 'LoginScreen.dart';
+import 'NewAccountScreen.dart';
+import 'SupportScreen.dart';
+
 import 'client/MenusScreen.dart';
 import 'client/OrdersScreen.dart';
 import 'client/PaymentScreen.dart';
 import 'client/SettingsScreen.dart';
-import 'LoginScreen.dart';
-import 'NewAccountScreen.dart';
+import 'client/TemplateMenuScreen.dart';
+
 import 'server/ServerMenusScreen.dart';
 import 'server/ScannerScreen.dart';
 import 'server/ServerOrdersScreen.dart';
 import 'server/BillingInfoScreen.dart';
+import 'server/QuantitiesOrderedScreen.dart';
 import 'server/ServerOrderHistory.dart';
+import 'server/MenuCategoryEdit.dart';
+import 'server/MenuEditCategoryList.dart';
+import 'server/MenuEditItemList.dart';
+import 'server/MenuItemEdit.dart';
+
 
 void main() => runApp(new MyApp());
 
@@ -27,18 +37,25 @@ class MyApp extends StatelessWidget {
 
       // TODO: Add to tree structure for navigating screens as needed
       routes: <String, WidgetBuilder>{
+        '/login': (BuildContext context) => LoginScreen(title: 'Login'),
+        '/newAccount': (BuildContext context) => NewAccountScreen(title: 'New Account'),
+        '/support': (BuildContext context) => SupportScreen(title: 'Support'),
+
+        '/client/menus': (BuildContext context) => MenusScreen(title: 'Menus'),
         '/client/orders': (BuildContext context) => OrdersScreen(title: 'Orders'),
         '/client/payment': (BuildContext context) => PaymentScreen(title: 'Payment'),
-        '/client/settings': (BuildContext context) =>
-            SettingsScreen(title: 'Settings'),
-        '/login': (BuildContext context) => LoginScreen(title: 'Login'),
-        '/client/menus': (BuildContext context) => MenusScreen(title: 'Menus'),
-        '/newAccount': (BuildContext context) => NewAccountScreen(title: 'New Account'),
+        '/client/settings': (BuildContext context) => SettingsScreen(title: 'Settings'),
+        '/client/menus/template': (BuildContext context) => TemplateMenuScreen(title: 'Template Menu'),
+
         '/server/menus': (BuildContext context) => ServerMenusScreen(title: 'Menus'),
         '/server/scanner': (BuildContext context) => ScannerScreen(title: 'Scanner'),
         '/server/orders': (BuildContext context) => ServerOrdersScreen(title: 'Orders'),
         '/server/billingInfo': (BuildContext context) => BillingInfoScreen(title: 'Billing Info'),
-        '/server/orderhistory': (BuildContext context) => OrderHistory(),
+        '/server/quantities': (BuildContext context) => QuantitiesOrderedScreen(title: 'Quantities'),
+        '/server/orderhistory': (BuildContext context) => ServerOrderHistory(title: 'Order History'),
+        '/server/editmenucategorylist': (BuildContext context) => MenuEditCategoryList(title: 'Edit Menu Category'),
+        '/server/editmenuitemlist': (BuildContext context) => MenuEditItemList(title: 'Edit Menu Items',),
+
       },
     );
   }
