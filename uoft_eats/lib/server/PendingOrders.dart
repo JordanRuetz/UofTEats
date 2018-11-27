2177529dbb9966ad501d18f9653daa0d19f91ac0
Anna Lieu <anna.lieu@mail.utoronto.ca>
2018-11-27 17:47:28 -0500

    Implemented pending orders to pull from database. Implemented complete order button with update to database.

--- uoft_eats/lib/server/PendingOrders.dart
+++ uoft_eats/lib/server/PendingOrders.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tuple/tuple.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 import 'package:uoft_eats/server/ServerDrawer.dart';
 import 'ReceiptHeaders.dart';
@@ -8,8 +10,9 @@ import 'FoodOrdered.dart';
 class PendingOrders extends StatefulWidget {
   final Map<int, Tuple2<String, Map<String, List>>> customerOrders;
   final double taxPercent;
+  AsyncSnapshot<QuerySnapshot> snapshot;
 
-  PendingOrders({Key key, this.customerOrders, this.taxPercent})
+  PendingOrders({Key key, this.customerOrders, this.taxPercent, this.snapshot})
       : super(key: key);
 
   @override
@@ -26,16 +29,17 @@ class _PendingOrders extends State<PendingOrders> {
         drawer: ServerDrawer(),
         body: OrderBody(
             customerOrders: widget.customerOrders,
-            taxPercent: widget.taxPercent));
-  }
+            taxPercent: widget.taxPercent, snapshot: widget.snapshot));
+    }
 }
 
 
 class OrderBody extends StatefulWidget {
   final Map<int, Tuple2<String, Map<String, List>>> customerOrders;
   double taxPercent;
+  AsyncSnapshot<QuerySnapshot> snapshot;
 
-  OrderBody({this.customerOrders, this.taxPercent});
+  OrderBody({this.customerOrders, this.taxPercent, this.snapshot});
 
   @override
   State<StatefulWidget> createState() {
@@ -46,18 +50,26 @@ class OrderBody extends StatefulWidget {
 class _OrderBodyState extends State<OrderBody> {
   Map<int, Tuple2<String, Map<String, List>>> customerOrders;
 
+  void _completeOrder(int order) {
+
+      for (int i = 0; i < widget.snapshot.data.documents.length; i++) {
+        if (widget.snapshot.data.documents[i]['orderNumber'] == order) {
+          var documentID = widget.snapshot.data.documents[i].documentID;
+          Firestore.instance.collection('orders').document(documentID).updateData({'status': 1});
+        }
+      }
+
+      Navigator.pushReplacementNamed(
+          context,'/server/orders');
+
+  }
+
   List<Widget> _getAllOrders() {
     customerOrders = widget.customerOrders;
     List<int> orderNumbers = (customerOrders).keys.toList();
     orderNumbers.sort();
 
     List<Widget> fullList = new List<Widget>();
-
-    fullList.add(new Row(
-      children: <Widget>[new RaisedButton(child: new Text("Refresh"), onPressed: () {setState(()
-      {});}
-      )]
-    ));
 
     for (int order in orderNumbers) {
       Tuple2<String, Map<String, List>> singleOrder = customerOrders[order];
@@ -78,7 +90,8 @@ class _OrderBodyState extends State<OrderBody> {
         ReceiptHeaders(),
         new Divider(color: Colors.blue),
         FoodOrdered(order: customerOrder, taxPercent: widget.taxPercent),
-      
+        new RaisedButton(child: new Text("Complete Order#" + order.toString()), onPressed: () {_completeOrder(order);}
+        ),
         new Divider(color: Colors.black87),
         new SizedBox(
           height: 12.0,
--- uoft_eats/lib/server/ServerOrdersScreen.dart
+++ uoft_eats/lib/server/ServerOrdersScreen.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:tuple/tuple.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'PendingOrders.dart';
 
 class ServerOrdersScreen extends StatefulWidget {
-  ServerOrdersScreen({Key key, this.title}) : super(key: key);
-
+  ServerOrdersScreen({Key key, this.title, this.name}) : super(key: key);
+  final String foodTruck = 'ideal catering';
   final String title;
-
+  final String name;
   static final double taxPercent = 0.13;
   // key is the order number. Map is unsorted
-  static final Map<int, Tuple2<String, Map<String, List>>> customerOrders = {
+ /* static final Map<int, Tuple2<String, Map<String, List>>> customerOrders = {
     215: new Tuple2<String, Map<String, List>>("Anna", {
       "Poutine": ["M", 1, 5.00],
       "Hot Dog": ["S", 1, 2.50],
@@ -31,7 +32,7 @@ class ServerOrdersScreen extends StatefulWidget {
       "Chicken Poutine": ["M", 1, 7.00],
       "Water": ["N/A", 1, 1.00]
     })
-  };
+  };*/
 
   @override
   _MyServerOrdersScreenState createState() => new _MyServerOrdersScreenState();
@@ -40,9 +41,56 @@ class ServerOrdersScreen extends StatefulWidget {
 class _MyServerOrdersScreenState extends State<ServerOrdersScreen> {
   @override
   Widget build(BuildContext context) {
-    return new PendingOrders(
-        customerOrders: ServerOrdersScreen.customerOrders,
-        taxPercent: ServerOrdersScreen.taxPercent);
+    //look at menuscreen
+    // pull at status 0
+    // change status to 1 when completed, status 2 when picked up
+    // two listviews for both statuse
+
+    Map<int, Tuple2<String, Map<String, List>>> customerOrders = new Map<int, Tuple2<String, Map<String, List>>>();
+
+    return StreamBuilder<QuerySnapshot>(
+      stream: Firestore.instance.collection('orders')
+          .where("status", isEqualTo: 0)
+          .where("server", isEqualTo: widget.foodTruck)
+          .snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (!snapshot.hasData) return const Text('Loading...');
+        final int count = snapshot.data.documents.length;
+        for (int i = 0; i < count; i++) {
+          //iterate each document(order)
+          Map<String, List> allItems = new Map<String, List>();
+          List orderQuery = snapshot.data.documents[i]['items'];
+          for (int j = 0; j < orderQuery.length; j++) {
+            List ItemInfo = new List(3);;
+            if (orderQuery[j]["size"] != "-1") {
+              ItemInfo[0] = orderQuery[j]["size"];
+            }
+            else {
+              ItemInfo[0] = "";
+            }
+            ItemInfo[1] = orderQuery[j]["quantity"];
+            ItemInfo[2] = orderQuery[j]["price"];
+
+            allItems[orderQuery[j]["type"]] = ItemInfo;
+
+
+            Tuple2<String, Map<String, List>> NameToOrder = Tuple2<String,
+                Map<String, List>>(
+                snapshot.data.documents[i]["client"], allItems);
+
+            customerOrders[snapshot.data.documents[i]['orderNumber']] =
+                NameToOrder;
+          }
+        }
+
+          return new PendingOrders(
+              customerOrders: customerOrders,
+              taxPercent: ServerOrdersScreen.taxPercent, snapshot: snapshot);
+
+      }
+
+    );
+
   }
 }
 
