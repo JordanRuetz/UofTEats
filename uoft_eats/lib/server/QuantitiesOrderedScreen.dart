441c1a41ec1a7f0024d9a68b369d302d14c826f0
Jordan Ruetz <jordan.ruetz@mail.utoronto.ca>
2018-11-27 01:38:21 -0500

    properly integrated item quantities screen with database

--- uoft_eats/lib/server/QuantitiesOrderedScreen.dart
+++ uoft_eats/lib/server/QuantitiesOrderedScreen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'package:uoft_eats/server/ServerDrawer.dart';
+import 'ItemQuantityTile.dart';
 
 class QuantitiesOrderedScreen extends StatefulWidget {
   QuantitiesOrderedScreen({Key key, this.title}) : super(key: key);
@@ -14,71 +15,56 @@ class QuantitiesOrderedScreen extends StatefulWidget {
 }
 
 class _MyQuantitiesOrderedScreenState extends State<QuantitiesOrderedScreen> {
-  List<NewMenuItemTile> menuItems = new List();
-
-  String server = "ideal catering";
+  static String server = "ideal catering";
+  static List<ItemQuantityTile> itemQuantities = new List();
 
   @override
   Widget build(BuildContext context) {
+    itemQuantities = new List();
     return new Scaffold(
         drawer: new ServerDrawer(),
         appBar: new AppBar(
           title: new Text(widget.title),
         ),
         body: new StreamBuilder<QuerySnapshot>(
-          stream: Firestore.instance.collection("orders").where("server", isEqualTo: server).snapshots(),
+          stream: Firestore.instance.collection("orders").snapshots(),
           builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
             if (snapshot.hasError) return new Text('Error: ${snapshot.error}');
             switch (snapshot.connectionState) {
               case ConnectionState.waiting:
                 return new Text('Loading...');
               default: {
-
-                  return NewMenuItemTile("something", 7, true);
-                }
-
-                //return NewMenuItemTile("something", 7, true);
+                setListView(snapshot.data.documents);
+                return new ListView.builder(
+                  itemCount: itemQuantities.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return itemQuantities[index];
+                  }
+                );
+              }
             }
           },
         ),
-
-/*        new ListView(
-          shrinkWrap: true,
-          padding: const EdgeInsets.all(20.0),
-          children:
-
-
-          <Widget>[
-            NewMenuItemTile('Poutine', 7, true),
-            NewMenuItemTile('Burger', 4, true),
-            NewMenuItemTile('Fries', 11, true),
-            NewMenuItemTile('Drink', 14, false),
-          ],
-        ));*/
-  }
-}
-
-class NewMenuItemTile extends StatefulWidget {
-  final String title;
-  int quantity;
-  final bool foodOrDrink;
-
-  NewMenuItemTile(this.title, this.quantity, this.foodOrDrink);
-
-  @override
-  _NewMenuItemTileState createState() => new _NewMenuItemTileState();
-}
-
-class _NewMenuItemTileState extends State<NewMenuItemTile> {
-  @override
-  Widget build(BuildContext context) {
-    return new ListTile(
-      leading: Icon(
-        widget.foodOrDrink ? Icons.fastfood : Icons.local_drink,
-      ),
-      title: new Text(widget.title),
-      trailing: new Text(widget.quantity.toString()),
-      onTap: () => setState(() => widget.quantity--),
     );
   }
+
+  void setListView(List<DocumentSnapshot> snapshots) {
+    for (DocumentSnapshot document in snapshots) {
+      for (Map map in document['items']) {
+        bool found = false;
+        for (ItemQuantityTile item in itemQuantities) {
+          if (map['type'] == item.getTitle() && map['size'] == item.getSize()) {
+            item.increaseQuantity(map['quantity']);
+            found = true;
+            break;
+          }
+        }
+        if (!found) {
+          ItemQuantityTile newMenuItem = ItemQuantityTile(map['type'], map['quantity'], map['size'], false);
+          _MyQuantitiesOrderedScreenState.itemQuantities.add(newMenuItem);
+        }
+      }
+    }
+  }
 }
+
