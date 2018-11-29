e1fc9f2f1db6d2ea48d2d0a247539f61b4d6e586
Wilbert Guo <guowilbert98@gmail.com>
2018-11-28 19:45:01 -0500

    Format order summary as table in PaymentConfirmationScreen.

--- uoft_eats/lib/client/PaymentConfirmationScreen.dart
+++ uoft_eats/lib/client/PaymentConfirmationScreen.dart
@@ -34,9 +34,6 @@ class PaymentConfirmation extends StatelessWidget {
   List<List> createOrder(Map myOrder){
 
     List<List> temp = [];
-//    myOrder.forEach((key, value) => (){
-//      temp.add(value);
-//    });
     var keys = myOrder.keys.toList();
     for(int i = 0; i < myOrder.length; i++){
       temp.add(myOrder[keys[i]]);
@@ -109,48 +106,104 @@ class OrderSummary extends StatelessWidget {
     : super(key: key);
 
   List order;
+  final double listSpacing = 12.0;
 
-  ExpansionTile _newMenuItemTile() {
-    return ExpansionTile(
-      leading: Icon(
-        Icons.fastfood,
-      ),
-      title: Text("Order Summary"),
-      backgroundColor: Colors.white70,
-      children: _generateItemTile(),
-    );
-  }
+  List<Widget> _getItems() {
+    List<String> listOfItems = [];
+    for(var i = 0; i < order.length; i++){
+      listOfItems.add(order[i][1].toString());
+    }
 
-  List<Widget> _generateItemTile() {
-    List<Widget> returnList = [];
-
-    for (int i = 0; i < order.length; i++) {
-      // Initialize 'order' with a key for every size + item combo
-      // corresponding to a value representing quantity of combo in order
-
+    List<Widget> orderWidgets = new List<Widget>();
+    for (var i = 0; i < listOfItems.length; i++) {
       if(order[i][0] > 0){
-        returnList.add(new ListTile(
-          trailing: new Row(
-            children: <Widget>[
-              new Text(order[i][1].toString()),
-              Spacer(flex: 1),
-              new Text(order[i][2].toString()),
-              Spacer(flex: 1),
-              new Text(order[i][0].toString()),
-              Spacer(flex: 1),
-              new Text('\$${(order[i][3] * order[i][0]).toStringAsFixed(2)}'),
-              Spacer(flex: 1),
-            ],
-          ),
-        ));
+        orderWidgets.add(new Container(
+          width: 120.0,
+          padding: new EdgeInsets.only(bottom: listSpacing),
+          child: new Text(listOfItems[i])));
       }
     }
-    return returnList;
+    return orderWidgets;
+  }
+
+  List<Widget> _getSizes() {
+    List<String> listOfSizes = [];
+    for(var i = 0; i < order.length; i++){
+      listOfSizes.add(order[i][2].toString());
+    }
+
+    List<Widget> orderWidgets = new List<Widget>();
+    for (var i = 0; i < listOfSizes.length; i++) {
+      if(order[i][0] > 0){
+        orderWidgets.add(new Container(
+          width: 120.0,
+          padding: new EdgeInsets.only(bottom: listSpacing),
+          child: new Text(listOfSizes[i])));
+      }
+    }
+    return orderWidgets;
+  }
+
+  List<Widget> _getQuantities() {
+    List<String> listOfQuantities = [];
+    for(var i = 0; i < order.length; i++){
+      listOfQuantities.add(order[i][0].toString());
+    }
+
+    List<Widget> orderWidgets = new List<Widget>();
+    for (var i = 0; i < listOfQuantities.length; i++) {
+      if(order[i][0] > 0){
+        orderWidgets.add(new Container(
+          width: 120.0,
+          padding: new EdgeInsets.only(bottom: listSpacing),
+          child: new Text(listOfQuantities[i])));
+      }
+    }
+    return orderWidgets;
+  }
+
+  List<Widget> _getPrices() {
+    List<String> listOfPrices = [];
+    for(var i = 0; i < order.length; i++){
+      listOfPrices.add('\$${(order[i][3] * order[i][0]).toStringAsFixed(2)}');
+    }
+
+    List<Widget> orderWidgets = new List<Widget>();
+    for (var i = 0; i < listOfPrices.length; i++) {
+      if(order[i][0] > 0){
+        orderWidgets.add(new Container(
+          width: 120.0,
+          padding: new EdgeInsets.only(bottom: listSpacing),
+          child: new Text(listOfPrices[i])));
+      }
+    }
+    return orderWidgets;
   }
 
   @override
   Widget build(BuildContext context) {
-    return new Container(child: _newMenuItemTile());
+    return new Container(
+      padding: new EdgeInsets.only(top: 15.0, bottom: 15.0),
+      child: new Column(
+        children: <Widget>[
+          new Row(
+            children: <Widget>[
+              new Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: _getItems()),
+              new Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: _getSizes()),
+              new Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: _getQuantities()),
+              new Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: _getPrices()),
+            ],
+          ),
+        ],
+      ));
   }
 }
 
