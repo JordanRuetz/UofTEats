e3176f93679fb65be919081dc469018af46cceb6
Wilbert Guo <guowilbert98@gmail.com>
2018-11-28 21:02:34 -0500

    Finalize styling for Payment Confirmation Screen.

--- uoft_eats/lib/client/PaymentConfirmationScreen.dart
+++ uoft_eats/lib/client/PaymentConfirmationScreen.dart
@@ -53,7 +53,7 @@ class PaymentConfirmation extends StatelessWidget {
             new PaymentConfirmationHeader(order: createOrder(order), subTotal: subTotal),
             new ReceiptHeaders(),
             new Divider(color: Colors.blue),
-            new OrderSummary(order: createOrder(order)),
+            new OrderSummary(order: createOrder(order), subTotal: subTotal),
             //Content
           ],
         )));
@@ -103,11 +103,12 @@ class PaymentConfirmationHeader extends StatelessWidget {
 }
 
 class OrderSummary extends StatelessWidget {
-  OrderSummary({Key key, this.order})
+  OrderSummary({Key key, this.order, this.subTotal})
     : super(key: key);
 
   List order;
   final double listSpacing = 12.0;
+  double subTotal;
 
   List<Widget> _getItems() {
     List<String> listOfItems = [];
@@ -203,6 +204,90 @@ class OrderSummary extends StatelessWidget {
                 children: _getPrices()),
             ],
           ),
+          new Divider(color: Colors.grey),
+          new Container(
+            height: 20.0,
+            child: new Row(
+              children: <Widget>[
+                new Container(
+                  width: 120.0,
+                  padding: new EdgeInsets.only(bottom: listSpacing, left: 20.0),
+                  margin: new EdgeInsets.only(right: 190.0),
+                  child: new Text(
+                    "Subtotal",
+                    style: new TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15.0
+                    )
+                  )
+                ),
+                new Container(
+                  padding: new EdgeInsets.only(bottom: listSpacing),
+                  child: new Text(""
+                    "\$" + subTotal.toStringAsFixed(2),
+                    style: new TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15.0
+                    )
+                  )
+                )
+              ],
+            )
+          ),
+          new Container(height: listSpacing,),
+          new Container(
+            height: 20.0,
+            child: new Row(
+              children: <Widget>[
+                new Container(
+                  width: 120.0,
+                  padding: new EdgeInsets.only(bottom: listSpacing, left: 20.0),
+                  margin: new EdgeInsets.only(right: 190.0),
+                  child: new Text(
+                    "Taxes",
+                    style: new TextStyle(
+                      fontSize: 15.0
+                    )
+                  )
+                ),
+                new Container(
+                  padding: new EdgeInsets.only(bottom: listSpacing),
+                  child: new Text("\$" + (subTotal * 0.13).toStringAsFixed(2))
+                )
+              ],
+            )
+          ),
+          new Divider(color: Colors.grey),
+          new Container(
+            height: 20.0,
+            child: new Row(
+              children: <Widget>[
+                new Container(
+                  width: 120.0,
+                  padding: new EdgeInsets.only(bottom: listSpacing, left: 20.0),
+                  margin: new EdgeInsets.only(right: 190.0),
+                  child: new Text(
+                    "Total",
+                    style: new TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15.0
+                    )
+                  )
+                ),
+                new Container(
+                  padding: new EdgeInsets.only(bottom: listSpacing),
+                  child: new Text(
+                    "\$" + (subTotal * 1.13).toStringAsFixed(2),
+                    style: new TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15.0
+                    )
+                  )
+                )
+              ],
+            )
+          ),
+
         ],
       ));
   }
